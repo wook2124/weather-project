db22520486cac179b2ef0f97395696380126253f
wook2124 <wook2124@naver.com>
2020-04-13 00:42:18 +0900

    #2.5 Conclusions

--- Weather.js
+++ Weather.js
@@ -8,45 +8,61 @@ const weatherOptions = {
   Thunderstorm: {
     iconName: "weather-lightning",
     gradient: ["#373B44", "#4286f4"],
+    title: "Thunderstorm in the house",
+    subtitle: "Actually, outside of the house"
   },
   Drizzle: {
     iconName: "weather-hail",
     gradient: ["#89F7FE", "#66A6FF"],
+    title: "Drizzle",
+    subtitle: "Is like rain, but gay 🏳️‍🌈"
   },
   Rain: {
     iconName: "weather-rainy",
     gradient: ["#00C6FB", "#005BEA"],
-    title: "Rainy",
-    subtitle: "It's raining. Make sure to bring your umbrella.",
+    title: "Raining like a MF",
+    subtitle: "For more info look outside"
   },
   Snow: {
     iconName: "weather-snowy",
     gradient: ["#7DE2FC", "#B9B6E5"],
+    title: "Cold as balls",
+    subtitle: "Do you want to build a snowman?⛄"
   },
   Atmosphere: {
     iconName: "weather-hail",
-    gradient: ["#89F7FE", "#66A6FF"],
+    gradient: ["#89F7FE", "#66A6FF"]
   },
   Clear: {
     iconName: "weather-sunny",
     gradient: ["#FF7300", "#FEF253"],
+    title: "Sunny as 🌞",
+    subtitle: "Go get your ass burnt"
   },
   Clouds: {
     iconName: "weather-cloudy",
     gradient: ["#D7D2CC", "#304352"],
+    title: "Clouds",
+    subtitle: "I know, it's boring ☁"
   },
   Mist: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "Mist!",
+    subtitle: "It's like you have no glasses on."
   },
   Dust: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "Dusty",
+    subtitle: "Thanks a lot China 😷"
   },
   Haze: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
-  },
+    title: "Haze",
+    subtitle: "Just don't go outside."
+  }
 };
 
 export default function Weather({ temp, condition }) {
@@ -110,14 +126,18 @@ const styles = StyleSheet.create({
     fontSize: 44,
     fontWeight: "300",
     marginBottom: 10,
+    textAlign: "left"
   },
   subtitle: {
+    fontWeight: "600",
     color: "white",
     fontSize: 24,
-    fontWeight: "600",
+    textAlign: "left"
   },
   textContainer: {
-    paddingHorizontal: 20,
     alignItems: "flex-start",
-  },
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    flex: 1
+  }
 });
